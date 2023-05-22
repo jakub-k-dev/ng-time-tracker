04db90ccbe429afa6c7becf989cec6ce7ee9c882
Jakub Kniebügl <jakub.kniebugl@gmail.com>
2023-05-22 13:38:46 +0200

    added basic pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TimeTableComponent } from './pages/time-table/time-table.component';
+import { FaqComponent } from './pages/faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/time-table', pathMatch: 'full' },
+  { path: 'time-table', component: TimeTableComponent },
+  { path: 'faq', component: FaqComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TimeTableComponent } from './pages/time-table/time-table.component';
+import { FaqComponent } from './pages/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeTableComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
