23e16be981651aa8c1773b595ebb08b6738fbc67
Jakub Kniebügl <jakub.kniebugl@gmail.com>
2023-05-23 14:48:28 +0200

    added basic layou and navbar,footer template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TimeTableComponent } from './pages/time-table/time-table.component';
 import { FaqComponent } from './pages/faq/faq.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimeTableComponent,
-    FaqComponent
+    FaqComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
